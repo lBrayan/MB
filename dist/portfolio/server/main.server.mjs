ef2e38518f97db0d16bf89bb9874379054fe7cc7
lBrayan <lbrayan.roa@gmail.com>
2024-11-24 01:40:58 -0500

    Ajuste Estilos

--- dist/portfolio/server/main.server.mjs
+++ dist/portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7XGMCI4Z.mjs";import"./chunk-2SWQX4PK.mjs";import"./chunk-UTRA2TPH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-WI4FOOOD.mjs";import"./chunk-2SWQX4PK.mjs";import"./chunk-UTRA2TPH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
